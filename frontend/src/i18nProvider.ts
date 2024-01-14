341bc2bf0ba6c999ca7e1994474b73f32a596399
11ai <124818466+11ai@users.noreply.github.com>
2024-01-15 01:27:57 +0200

    created on Mon Jan 15 2024 01:27:44 GMT+0200 (Eastern European Standard Time)

---
+++ frontend/src/i18nProvider.ts
@@ -0,0 +1,17 @@
+// in i18nProvider.js
+    import { mergeTranslations } from "ra-core";
+    import polyglotI18nProvider from "ra-i18n-polyglot";
+    import enOriginal from 'ra-language-english';
+
+    const enResources = { resources: {"Apps":{"name":"Apps","fields":{"Appname":"Appname","Developer":"Developer","Category":"Category","Numberofentities":"Numberofentities","Id":"Id"}}}};
+
+
+    const en = mergeTranslations(enOriginal,enResources);
+
+    const translations = { en};
+    export const i18nProvider = polyglotI18nProvider(
+      (locale) => translations[locale],
+      "en", //default locale
+      [{"locale":"en","name":"English"}]
+    );
+    
