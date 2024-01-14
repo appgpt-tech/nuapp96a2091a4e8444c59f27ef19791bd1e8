09805e3b7ee8a64e87341a6e86de0c4892f6bde2
11ai <124818466+11ai@users.noreply.github.com>
2024-01-15 01:27:55 +0200

    created on Mon Jan 15 2024 01:27:44 GMT+0200 (Eastern European Standard Time)

---
+++ frontend/src/dashboard.tsx
@@ -0,0 +1,7 @@
+import { Card, CardContent, CardHeader } from "@mui/material";
+    export const Dashboard = () => (
+      <Card>
+      <CardHeader title= "nuapp" />
+      <CardContent> it is a registry of apps < /CardContent>
+      </Card>
+    );
